e9a9d58c5f7368906c426d9fd394579d22784cf7
DevKass <141834141+Devkass7@users.noreply.github.com>
2026-09-15 17:52:12 +0000

    Initial commit

---
+++ src/menu.js
@@ -0,0 +1,59 @@
+import logo from "./images/logo.png"
+import jollof from "./images/jollof.jpg"
+import fries from "./images/fries.jpg"
+import ayoyo from "./images/ayoyo.jpg"
+import pizza from "./images/pizza.jpg"
+
+
+ function menu(){
+    const menu = document.getElementById("container");
+    menu.innerHTML =""
+
+    const menuItems = document.createElement("div")
+
+    document.querySelector(".hero").innerHTML = "<h1>Your Meal is Just a <span>Click</span> away</h1>"
+
+      
+    menuItems.innerHTML = `<main>
+<fieldset>
+    <legend>
+        <img class="logo" src="${logo}" alt="logo" />
+    </legend>
+    <div>
+        <h3>Assorted Jollof Rice</h3>
+        <img class="food-image" src="${jollof}" alt="" /><button class="cta-button">Place Order</button>
+    </div>
+</fieldset>
+
+<fieldset>
+    <legend><img class="logo" src="${logo}" alt="logo" /></legend>
+    <div>
+        <h3>Special Banku</h3>
+        <img class="food-image" src="${ayoyo}" alt="" /><button class="cta-button">Place Order</button>
+    </div>
+</fieldset>
+
+<fieldset>
+    <legend><img class="logo" src="${logo}" alt="logo" /></legend>
+    <div>
+        <h3>Irresistible Fries</h3>
+        <img class="food-image" src="${fries}" alt="" /><button class="cta-button">Place Order</button>
+    </div>
+</fieldset>
+
+<fieldset>
+    <legend><img class="logo" src="${logo}" alt="logo" /></legend>
+    <div>
+        <h3>Amazing Pizza</h3>
+        <img class="food-image" src="${pizza}" alt="" /><button class="cta-button">Place Order</button>
+    </div>
+</fieldset></main>
+`
+
+   // menuItems.innerHTML = `<img scr='${logo}' >`
+    menu.appendChild(menuItems)
+}
+
+const items = menu;
+
+export default items;
